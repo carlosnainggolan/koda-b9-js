5312ae5e4e20575b89c40cb9f24d77e0f9548eb9
carlosnainggolan <145370056+carlosnainggolan@users.noreply.github.com>
2026-07-15 11:51:38 +0700

    Add files via upload

---
+++ minitask1.js
@@ -0,0 +1,27 @@
+const who = {
+  am: {
+    i: "Carlos Nainggolan"
+  }
+}
+console.log(who.am.i)
+
+const hello = {
+  world: "Hello world"
+}
+console.log(hello.world)
+
+
+const obj = {str : [, [, , [, , , {koda : [
+  {
+    tech: {
+      academy: "Koda Tech Academy"
+    }
+  }
+]}]]]}
+
+console.log(obj.str[1][2][3].koda[0].tech["academy"])
+
+const my = [{ very: { best: [ , { sport: { is: "Footbal" } }]}}]
+
+console.log(my[0].very['best'][1].sport.is)
+
