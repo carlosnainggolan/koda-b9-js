ce74a1006ec2715850cf2b722fd85ed05a625076
carlosnainggolan <145370056+carlosnainggolan@users.noreply.github.com>
2026-07-15 16:08:45 +0700

    Add files via upload

---
+++ minitask3.js
@@ -0,0 +1,51 @@
+let kembali = 1000000
+let limapuluh = 0
+let sepuluh = 0
+let lima = 0
+let dua = 0
+let satu = 0
+
+// while (kembali > 0) {
+//   if (kembali - 50000 >= 0) {
+//     kembali -= 50000
+//     limapuluh++
+//   } else if (kembali - 10000 >= 0) {
+//     kembali -= 10000
+//     sepuluh++
+//   } else if (kembali - 5000 >= 0) {
+//     kembali -= 5000
+//     lima++
+//   } else if (kembali - 2000 >= 0) {
+//     kembali -= 2000
+//     dua++
+//   } else if (kembali - 1000 >= 0) {
+//     kembali -= 1000
+//     satu++
+//   }
+// }
+
+for (;kembali;) {
+  if (kembali - 50000 >= 0) {
+    kembali -= 50000
+    limapuluh++
+  } else if (kembali - 10000 >= 0) {
+    kembali -= 10000
+    sepuluh++
+  } else if (kembali - 5000 >= 0) {
+    kembali -= 5000
+    lima++
+  } else if (kembali - 2000 >= 0) {
+    kembali -= 2000
+    dua++
+  } else if (kembali - 1000 >= 0) {
+    kembali -= 1000
+    satu++
+  }
+}
+
+console.log(`50rb = ${limapuluh}`)
+console.log(`10rb = ${sepuluh}`)
+console.log(`5rb = ${lima}`)
+console.log(`2rb = ${dua}`)
+console.log(`1rb = ${satu}`)
+
