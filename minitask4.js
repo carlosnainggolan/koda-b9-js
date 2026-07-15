4d5d52888a92dd20c74f4ff2e54397c433690b0e
carlosnainggolan <145370056+carlosnainggolan@users.noreply.github.com>
2026-07-15 17:01:36 +0700

    Add files via upload

---
+++ minitask4.js
@@ -0,0 +1,43 @@
+const array = [
+  [1], 
+  [2], 
+  {
+    name: 'carlos'
+  },
+  {
+    tanggal: '20'
+  },
+  'carlos',
+  'brandsma',
+  'nainggolan',
+  true, 
+  9
+]
+
+let number = 0
+let string = 0
+let boolean = 0
+let object = 0
+let array1 = 0
+
+for (let i = 0; i < array.length; i++) {
+  if (typeof array[i] === 'number') {
+    number++
+  } else if (typeof array[i] === 'string') {
+    string++
+  } else if (typeof array[i] === 'boolean') {
+    boolean++
+  } else if (array[i] instanceof Object && Array.isArray(array[i])) {
+    object++
+  } else if (Array.isArray(array[i])) {
+    array1++
+  }
+}
+
+console.log(`
+    Number = ${number},
+    String = ${string},
+    boolean = ${boolean},
+    Object = ${object},
+    array1 = ${array1}
+  `)
