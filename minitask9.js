4639983e7b2576ace6ea00b49182706cf507d336
carlosnainggolan <145370056+carlosnainggolan@users.noreply.github.com>
2026-07-16 17:14:14 +0700

    Add files via upload

---
+++ minitask9.js
@@ -0,0 +1,17 @@
+function proccessArray (arr, processor) {
+  const result = []
+  for (let i = 0; i < arr.length; i++ ) {
+    result = processor(arr[i])
+  }
+  return result
+}
+
+const showNumber = num => `Number ${num}`
+const makeDollar = num => `$${num}`
+const addFive = num => num + 5
+
+const numbers = [1, 2, 3, 4, 5]
+
+console.log(proccessArray(numbers, showNumber))
+console.log(proccessArray(numbers, makeDollar))
+console.log(proccessArray(numbers, addFive))
